0f4e4b12057797e214ec93cab5644d2bdcd989db
Thanai <thanaitrunks@gmail.com>
2024-05-31 22:00:16 +0530

    let us check

---
+++ gitwithjenkins/Result.java
@@ -0,0 +1,13 @@
+package gitwithjenkins;
+
+public class Result {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***MESSAGE FROM THANAI***");
+		System.out.println("This file has been executed by the jenkins ");
+		System.out.println("CONGRATS");
+
+	}
+
+}
